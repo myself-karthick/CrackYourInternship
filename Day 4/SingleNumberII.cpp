49c4c9eb0fbfc29b692cb95c31a5782a8ca6dc26
Karthick M <kartthickmohan@gmail.com>
2023-07-04 13:31:18 +0530

    Day 4 Push

---
+++ Day 4/SingleNumberII.cpp
@@ -0,0 +1,25 @@
+//Link : https://leetcode.com/problems/single-number-ii/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int ans=0;
+        for(int i=0; i<32; i++)
+        {
+            int sum=0;
+            for(int j=0; j<nums.size(); j++)
+            {
+                if((nums[j]>>i)&1==1)
+                {
+                    sum++;
+                    sum=sum%3;
+                }
+            }
+            if(sum!=0){
+                ans = ans | (sum<<i);
+            }
+        }
+        return ans;
+    }
+};
