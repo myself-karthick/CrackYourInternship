b314826e63b63d009f55467e284ac2fc939c1f55
Karthick M <kartthickmohan@gmail.com>
2023-08-08 22:53:12 +0530

    Aug/7/23 Push

---
+++ August/Day 7/Searcha2DMatrix.cpp
@@ -0,0 +1,19 @@
+// Link : https://leetcode.com/problems/search-a-2d-matrix/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n=mat.size(), m=mat[0].size();
+        
+        for(int i=0,j=m-1;i<n && j>=0;){
+            int x=mat[i][j];
+            if(x==target){
+                return true;
+            }else if(x>target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;
+    }
+};
